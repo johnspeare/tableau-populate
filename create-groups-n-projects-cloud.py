01bb9768022428adb9cbabd1546560b5c2994ad5
johnspeare <john@phred.org>
2023-08-16 13:33:17 -0700

    first set

---
+++ create-groups-n-projects-cloud.py
@@ -0,0 +1,138 @@
+import random
+import subprocess
+import csv
+import xml.etree.ElementTree as ET
+
+#cloud-PAT
+#server_url = 'https://us-west-2a.online.tableau.com/'
+#site_name = johndogfood
+#name: cloudrunner
+#key: mVqxFjuKQgKea6IEQ7kS9Q==:B1DNQ5DRfPRCPQo3vSV6ttvoTzXBeWU7
+
+#server-PAT
+#name: scriptrunner
+#key: djL7AwHJTGC87JNo740LoQ==:oloG8SJdzG3CbMUQdczVD44bDsqly7x1
+#server_url = 'https://edg3.cyclingspokane.com'
+
+# User-defined variables
+num_groups = 2
+server_url = 'https://us-west-2a.online.tableau.com/'
+site_name = 'johndogfood'
+PAT_name = 'cloudrunner'
+PAT_value = '8NWdr5EPS9uWOJ0GGcOEFQ==:aAS0bNrkVY5VGjgNP7KbUVN63rcJLGE7'
+csv_file_path = 'users-cloud.csv'
+api_version = "3.4"
+num_projects = 4
+
+# Define the group names
+groups = []
+for i in range(num_groups):
+    group_name = f'Group{i+1}'
+    groups.append(group_name)
+
+# Authenticate with the remote Tableau Server
+login_cmd = f'tabcmd login -s {server_url} -t "{site_name}" --token-name "{PAT_name}" --token-value "{PAT_value}"'
+subprocess.call(login_cmd, shell=True)
+
+# Read usernames from the CSV file
+usernames = []
+with open(csv_file_path, 'r') as csv_file:
+    reader = csv.reader(csv_file)
+    for row in reader:
+        username = row[0].strip()
+        if username:
+            usernames.append(username)
+
+# Shuffle the usernames randomly
+random.shuffle(usernames)
+
+# Calculate the number of users per group
+users_per_group = len(usernames) // len(groups)
+
+# Iterate over the groups
+for i, group in enumerate(groups):
+    # Create the group
+    create_group_cmd = f'tabcmd creategroup "{group}"'
+    subprocess.call(create_group_cmd, shell=True)
+    
+    # Get the users for the current group
+    group_users = random.sample(usernames, users_per_group)
+    
+    # Remove the assigned users from the list
+    for user in group_users:
+        usernames.remove(user)
+    
+    # Write group users to a temporary text file
+    temp_text_file_path = f'{group}_users.txt'
+    with open(temp_text_file_path, 'w') as temp_file:
+        temp_file.write('\n'.join(group_users))
+    
+    # Add the users to the group
+    add_users_cmd = f'tabcmd addusers "{group}" --users "{temp_text_file_path}"'
+    subprocess.call(add_users_cmd, shell=True)
+
+    # Remove the temporary text file
+    subprocess.call(f'rm "{temp_text_file_path}"', shell=True)
+
+print("Groups created successfully!")
+
+# ######### CREATE PROJECTS ###############
+
+# Step 1: Generate signin.xml file
+signin_xml = f'''<tsRequest>
+  <credentials personalAccessTokenName="{PAT_name}"
+    personalAccessTokenSecret="{PAT_value}" >
+      <site contentUrl="{site_name}" />
+  </credentials>
+</tsRequest>'''
+
+with open('signin.xml', 'w') as file:
+    file.write(signin_xml)
+
+# Step 2: Sign in to Tableau Server and retrieve site-id and credentials-token
+signin_command = f'curl "{server_url}/api/{api_version}/auth/signin" -X POST -d "@signin.xml"'
+signin_output = subprocess.check_output(signin_command, shell=True, encoding='utf-8')
+
+# Parse the XML response to extract site-id and credentials-token
+namespaces = {'t': 'http://tableau.com/api'}
+root = ET.fromstring(signin_output)
+
+site_element = root.find('.//t:site', namespaces)
+if site_element is None:
+    print("Error: Site element not found in the XML response.")
+    exit(1)
+
+site_id = site_element.attrib.get('id')
+if site_id is None:
+    print("Error: Site ID attribute not found in the XML response.")
+    exit(1)
+
+credentials_element = root.find('.//t:credentials', namespaces)
+if credentials_element is None:
+    print("Error: Credentials element not found in the XML response.")
+    exit(1)
+
+credentials_token = credentials_element.attrib.get('token')
+if credentials_token is None:
+    print("Error: Credentials token attribute not found in the XML response.")
+    exit(1)
+
+# Step 3: Create projects in Tableau Server
+project_xml_template = '''<tsRequest>
+    <project
+      name="{ProjectName}"
+      description="This is a new project number {n}" />
+</tsRequest>'''
+
+# Loop to create projects
+for n in range(1, num_projects + 1):
+    project_name = f"Project{n}"
+    create_project_xml = project_xml_template.format(ProjectName=project_name, n=n)
+    
+    create_project_command = (
+        f'curl "{server_url}/api/{api_version}/sites/{site_id}/projects" '
+        f'-X POST -H "X-Tableau-Auth:{credentials_token}" -d "@-"'
+    )
+    
+    subprocess.run(create_project_command, shell=True, input=create_project_xml.encode('utf-8'))
+
---
+++ sign-in.py
@@ -0,0 +1,131 @@
+import subprocess
+import xml.etree.ElementTree as ET
+
+# User-defined variables
+server_url = "https://edg3.cyclingspokane.com"
+PAT_name = "scriptrunner"
+PAT_value = "djL7AwHJTGC87JNo740LoQ==:oloG8SJdzG3CbMUQdczVD44bDsqly7x1"
+api_version = "3.4"
+
+# Step 1: Generate signin.xml file
+signin_xml = f'''<tsRequest>
+  <credentials personalAccessTokenName="{PAT_name}"
+    personalAccessTokenSecret="{PAT_value}" >
+      <site contentUrl="" />
+  </credentials>
+</tsRequest>'''
+
+with open('signin.xml', 'w') as file:
+    file.write(signin_xml)
+
+# Step 2: Sign in to Tableau Server and retrieve site-id and credentials-token
+signin_command = f'curl "{server_url}/api/{api_version}/auth/signin" -X POST -d "@signin.xml"'
+print(f"REST Command: {signin_command}")
+
+signin_output = subprocess.check_output(signin_command, shell=True, encoding='utf-8')
+print(f"Response:\n{signin_output}")
+
+# Parse the XML response to extract site-id and credentials-token
+namespaces = {'t': 'http://tableau.com/api'}
+root = ET.fromstring(signin_output)
+
+site_element = root.find('.//t:site', namespaces)
+if site_element is None:
+    print("Error: Site element not found in the XML response.")
+    exit(1)
+
+site_id = site_element.attrib.get('id')
+if site_id is None:
+    print("Error: Site ID attribute not found in the XML response.")
+    exit(1)
+
+credentials_element = root.find('.//t:credentials', namespaces)
+if credentials_element is None:
+    print("Error: Credentials element not found in the XML response.")
+    exit(1)
+
+credentials_token = credentials_element.attrib.get('token')
+if credentials_token is None:
+    print("Error: Credentials token attribute not found in the XML response.")
+    exit(1)
+
+# Step 3: Query Tableau Server to get group information
+groups_url = f'{server_url}/api/{api_version}/sites/{site_id}/groups'
+headers = {'X-Tableau-Auth': credentials_token}
+groups_command = f'curl "{groups_url}" -H "X-Tableau-Auth: {credentials_token}"'
+print(f"\nREST Command: {groups_command}")
+
+groups_output = subprocess.check_output(groups_command, shell=True, encoding='utf-8')
+print(f"\nGroup Information:\n{groups_output}")
+
+# Parse the XML response to extract group IDs and names
+groups_root = ET.fromstring(groups_output)
+groups = groups_root.findall('.//t:group', namespaces)
+
+if len(groups) > 0:
+    print("\nGroup IDs and Names:")
+    for group in groups:
+        group_id = group.attrib.get('id')
+        group_name = group.attrib.get('name')
+        print(f"Group ID: {group_id}\tGroup Name: {group_name}")
+else:
+    print("No groups exist on this server.")
+
+# Step 4: Query Tableau Server to get project names and IDs
+projects_url = f'{server_url}/api/{api_version}/sites/{site_id}/projects'
+projects_command = f'curl "{projects_url}" -H "X-Tableau-Auth: {credentials_token}"'
+print(f"\nREST Command: {projects_command}")
+
+projects_output = subprocess.check_output(projects_command, shell=True, encoding='utf-8')
+print(f"\nProject Information:\n{projects_output}")
+
+# Parse the XML response to extract project IDs and names
+projects_root = ET.fromstring(projects_output)
+projects = projects_root.findall('.//t:project', namespaces)
+
+if len(projects) > 0:
+    print("\nProject IDs and Names:")
+    for project in projects:
+        project_id = project.attrib.get('id')
+        project_name = project.attrib.get('name')
+        print(f"Project ID: {project_id}\tProject Name: {project_name}")
+else:
+    print("No projects exist on this server.")
+
+# Print the Site ID and Credentials Token
+print(f"\nSite ID: {site_id}")
+print(f"Credentials Token: {credentials_token}")
+
+# Step 5: Check if test projects or groups exist
+test_projects_exist = any(project.attrib.get('name', '').startswith("Project") for project in projects)
+test_groups_exist = any(group.attrib.get('name', '').startswith("Group") for group in groups)
+
+if test_projects_exist or test_groups_exist:
+    prompt = input("\nTo delete the test Projects and Groups, enter 'delete': ")
+
+    if prompt.lower() == "delete":
+        # Delete test projects
+        for project in projects:
+            project_name = project.attrib.get('name')
+            if project_name.startswith("Project"):
+                project_id = project.attrib.get('id')
+                delete_project_url = f'{server_url}/api/{api_version}/sites/{site_id}/projects/{project_id}'
+                delete_project_command = f'curl "{delete_project_url}" -X DELETE -H "X-Tableau-Auth: {credentials_token}"'
+                subprocess.run(delete_project_command, shell=True)
+                print(f"\nDeleted Project: {project_name}")
+
+        # Delete test groups
+        for group in groups:
+            group_name = group.attrib.get('name')
+            if group_name.startswith("Group"):
+                group_id = group.attrib.get('id')
+                delete_group_url = f'{server_url}/api/{api_version}/sites/{site_id}/groups/{group_id}'
+                delete_group_command = f'curl "{delete_group_url}" -X DELETE -H "X-Tableau-Auth: {credentials_token}"'
+                subprocess.run(delete_group_command, shell=True)
+                print(f"\nDeleted Group: {group_name}")
+
+        print("\nDeletion completed.")
+    else:
+        print("\nDeletion not performed.")
+else:
+    print("\nNo test projects or groups exist on this server.")
